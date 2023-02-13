e1a43d7c3d20bf9b6a080f3857e957937adcbc7c
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-02-13 18:25:31 +0530

    both buttons are worked

--- Udemi build ios app/ViewController.swift
+++ Udemi build ios app/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func alertaction(_ sender: UIButton) {
+        let alert = UIAlertController(title: "MyTital", message: "my massage Here", preferredStyle: .alert)
+        present(alert,animated: true,completion: nil)
+    }
+    
+    @IBAction func actionsheet(_ sender: UIButton) {
+        let sheet = UIAlertController(title: "MyTital", message: "my massage Here", preferredStyle: .actionSheet)
+            present(sheet,animated: true,completion: nil)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
