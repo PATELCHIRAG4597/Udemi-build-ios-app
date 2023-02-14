93f4fb2a75a17412b69fd0672dba3606ab4093b8
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-02-14 10:37:56 +0530

    Alert And ActionSheet demo complete(1)

--- Udemi build ios app/ViewController.swift
+++ Udemi build ios app/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 class ViewController: UIViewController {
     @IBAction func alertaction(_ sender: UIButton) {
         let alert = UIAlertController(title: "MyTital", message: "my massage Here", preferredStyle: .alert)
+        let cancelbuttone = UIAlertAction(title: "cancelbuttone", style: .cancel) {(action) in
+            print("cancel button")
+        }
+        alert.addAction(cancelbuttone)
         present(alert,animated: true,completion: nil)
     }
-    
     @IBAction func actionsheet(_ sender: UIButton) {
         let sheet = UIAlertController(title: "MyTital", message: "my massage Here", preferredStyle: .actionSheet)
+        let cancelbuttone = UIAlertAction(title: "cancelbuttone", style: .cancel) {(action) in
+            print("cancel button")
+        }
+
+        sheet.addAction(cancelbuttone)
             present(sheet,animated: true,completion: nil)
     }
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
